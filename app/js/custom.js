8940523a512564a89590c15acac9d71812319b22
Maclean, James <James.Maclean@fil.com>
2016-12-08 12:03:02 +0000

    black styling

--- app/js/custom.js
+++ app/js/custom.js
@@ -2,15 +2,15 @@
 
 // $(document).ready(function() {
 //
-//     $('.errorMarker').scrollToFixed( {
+//     $('.msg').scrollToFixed( {
 //       bottom: 0,
-//       limit: $('.errorMarker').offset().top,
+//       limit: $('.msg').offset().top,
 //       preFixed: function() { $(this).removeClass('hide'); },
 //       postFixed: function() { $(this).addClass('hide'); }
 //     });
-//   $('.errorMarker').scrollToFixed( {
+//   $('.msg').scrollToFixed( {
 //     top: 0
-//     // limit: $('.errorMarker').offset().top
+//     // limit: $('.msg').offset().top
 //     // preFixed: function() { $(this).removeClass('hide'); },
 //     // postFixed: function() { $(this).addClass('hide'); }
 //   });
@@ -30,8 +30,8 @@ leftBtn.on( 'click', function (e) {
     checkTabPosition();
     console.log(tabPosition);
     // if (tabPosition >= 5 ){
-    //   $('.buttonWrap--errorContainer').removeClass('contains-error');
-    //   console.log('removed error');
+    //   $('.buttonWrap--msgContainer').removeClass('contains-msg');
+    //   console.log('removed msg');
     // }
   }
   else {
@@ -54,8 +54,8 @@ rightBtn.on( 'click', function (e) {
     console.log(tabPosition);
 
     // if (tabPosition <= 4 ){
-    //   $('.buttonWrap--errorContainer').addClass('contains-error');
-    //   console.log('added error');
+    //   $('.buttonWrap--msgContainer').addClass('contains-msg');
+    //   console.log('added msg');
     // }
   }
   else {
@@ -71,18 +71,18 @@ rightBtn.on( 'click', function (e) {
 
 function checkTabPosition() {
   if (tabPosition == 5) {
-      console.log("error is right");
-      TweenMax.to( $('.tabGroup--errorContainer__right'), 0.3, { opacity: 1 });
+      console.log("msg is right");
+      TweenMax.to( $('.tabGroup--msgContainer__right'), 0.3, { opacity: 1 });
   }
 
   else if (tabPosition == 11) {
-    console.log("error is left");
-    TweenMax.to( $('.tabGroup--errorContainer__left'), 0.3, { opacity: 1 });
+    console.log("msg is left");
+    TweenMax.to( $('.tabGroup--msgContainer__left'), 0.3, { opacity: 1 });
   }
   else if (tabPosition >= 6 &&  tabPosition <= 10) {
     console.log("on screen");
-    TweenMax.to( $('.tabGroup--errorContainer__left'), 0.3, { opacity: 0 });
-    TweenMax.to( $('.tabGroup--errorContainer__right'), 0.3, { opacity: 0 });
+    TweenMax.to( $('.tabGroup--msgContainer__left'), 0.3, { opacity: 0 });
+    TweenMax.to( $('.tabGroup--msgContainer__right'), 0.3, { opacity: 0 });
   }
 
 };
@@ -92,59 +92,59 @@ function checkTabPosition() {
 // var tabsInView = 5
 // var amount of tabs $('.tab').length;
 
-//var error position=10
+//var msg position=10
 // 14 is on the righ
-// 10 is on the left errorPosition + (tabsInView - 1);
-// distance to error Currentposition
+// 10 is on the left msgPosition + (tabsInView - 1);
+// distance to msg Currentposition
 
 
 
-// Move to error tab
+// Move to msg tab
 //Is it off to the left?
-//- scroll untill the error is on the left
-// - show error marker right -1160
+//- scroll untill the msg is on the left
+// - show msg marker right -1160
 // postion 10
 
 //Is it off to the right?
-//- scroll untill the error is on the right
-// - show error marker left -2088
+//- scroll untill the msg is on the right
+// - show msg marker left -2088
 // position = 6
-$( document ).on('click', '.js-tabError-left-trigger', function(e) {
+$( document ).on('click', '.js-tabmsg-left-trigger', function(e) {
   TweenMax.to(scrollTabs, 0.3, { x: '-2088px' });
   tabPosition = 10;
   checkTabPosition();
   e.preventDefault();
 } );
 
-$( document ).on('click', '.js-tabError-right-trigger', function(e) {
+$( document ).on('click', '.js-tabmsg-right-trigger', function(e) {
   TweenMax.to(scrollTabs, 0.3, { x: '-1160px' });
   tabPosition = 6;
   checkTabPosition();
   e.preventDefault();
 } );
 
-// Check errorTab position
+// Check msgTab position
 
 // is it off to the left? Currentposition < 6
-// - hide tab error
+// - hide tab msg
 
 // is it off to the right? Currentposition > 10
-// - hide tab error hide tab error
+// - hide tab msg hide tab msg
 
  // is it on screen now? (Currentposition <=6 && Currentposition >=10)
-// - hide the error markers
-// -fade in and move up tab error
+// - hide the msg markers
+// -fade in and move up tab msg
 
 
-$('.js-show-error-trigger').on('mouseenter', function(e) {
-  TweenMax.to( $('.js-hide-error-target'), 0.6, {
+$('.js-show-msg-trigger').on('mouseenter', function(e) {
+  TweenMax.to( $('.js-hide-msg-target'), 0.6, {
     x: '0%',
     ease: Power2.easeOut
   }  );
 });
 
-$('.js-hide-error-trigger').on('mouseleave', function(e) {
-  TweenMax.to( $('.js-hide-error-target'), 0.6, {
+$('.js-hide-msg-trigger').on('mouseleave', function(e) {
+  TweenMax.to( $('.js-hide-msg-target'), 0.6, {
     x: '-100%',
     ease: Power2.easeIn
   }  );
@@ -179,20 +179,20 @@ hideShow();
 console.log("addInvestment");
       e.preventDefault();
 });
-$('.js-error-offscreen-trigger').on('inview', function(event, isInView) {
+$('.js-msg-offscreen-trigger').on('inview', function(event, isInView) {
   if (isInView) {
 console.log('inview');
 
-    TweenMax.to( $('.js-error-offscreen-target'), 0.4, {
+    TweenMax.to( $('.js-msg-offscreen-target'), 0.4, {
       y: '-200%',
       autoAlpha: 0,
       ease: Power2.easeIn
-    }, function() {  $('.js-error-offscreen-target').removeClass('show');} );
+    }, function() {  $('.js-msg-offscreen-target').removeClass('show');} );
 
   } else {
     console.log('OUTview');
-    $('.js-error-offscreen-target').addClass('show');
-    TweenMax.to( $('.js-error-offscreen-target'), 0.6, {
+    $('.js-msg-offscreen-target').addClass('show');
+    TweenMax.to( $('.js-msg-offscreen-target'), 0.6, {
       y: '0%',
       autoAlpha: 1,
       ease: Power2.easeOut
@@ -203,11 +203,15 @@ console.log('inview');
 
 // ease: Power2.easeOut
 
- $( document ).on('click', '.js-scrollToError', function( e ) {
+ $( document ).on('click', '.js-scrollTomsg', function( e ) {
   TweenMax.to(window, 1, {scrollTo:{y:"#section1", offsetY:120}});
-  //  TweenMax.to(window, 2, {scrollTo:{y:".js-error-offscreen-trigger", offsetY:50}} );
+  //  TweenMax.to(window, 2, {scrollTo:{y:".js-msg-offscreen-trigger", offsetY:50}} );
    e.preventDefault();
  } );
 
-
+ $( document ).on('click', '.js-scrollToTable', function( e ) {
+  TweenMax.to(window, 1, {scrollTo:{y:1600}});
+  //  TweenMax.to(window, 2, {scrollTo:{y:".js-msg-offscreen-trigger", offsetY:50}} );
+   e.preventDefault();
+ } );
 // $( ".tab__active" ).clone().appendTo( ".tabGroup" );
