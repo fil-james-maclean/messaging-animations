56c5e4850b6a7110a083bd954741463ab1550807
Maclean, James <James.Maclean@fil.com>
2017-01-20 14:57:07 +0000

    Revert "tooltip hover effect"
    This reverts commit 1475906cc29a902d4876ff922c84068b16dc7599.
    
    # Conflicts:
    #	app/css/main.css
    #	app/index.html
    #	app/scss/main.scss

--- app/js/custom.js
+++ app/js/custom.js
@@ -136,28 +136,19 @@ $( document ).on('click', '.js-tabmsg-right-trigger', function(e) {
 // -fade in and move up tab msg
 
 
-// $('.js-show-msg-trigger').on('mouseenter', function(e) {
-//   TweenMax.to( $('.js-hide-msg-target'), 0.6, {
-//     x: '0%',
-//     ease: Power2.easeOut
-//   }  );
-// });
-//
-// $('.js-hide-msg-trigger').on('mouseleave', function(e) {
-//   TweenMax.to( $('.js-hide-msg-target'), 0.6, {
-//     x: '-100%',
-//     ease: Power2.easeIn
-//   }  );
-// });
-
 $('.js-show-msg-trigger').on('mouseenter', function(e) {
-    $('.js-hide-msg-target').removeClass('hide');
+  TweenMax.to( $('.js-hide-msg-target'), 0.6, {
+    x: '0%',
+    ease: Power2.easeOut
+  }  );
 });
 
 $('.js-hide-msg-trigger').on('mouseleave', function(e) {
-    $('.js-hide-msg-target').addClass('hide');
+  TweenMax.to( $('.js-hide-msg-target'), 0.6, {
+    x: '-100%',
+    ease: Power2.easeIn
+  }  );
 });
-
 
 // TweenMax.to( $(this), 0.3, {
 //   y:'0%',
